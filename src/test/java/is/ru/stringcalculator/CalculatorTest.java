36368bb3842547d8d95f970340a543d6a3c7f83f
Ólafur Helgi Jónsson <olafur9@gmail.com>
2014-10-17 15:50:54 +0000

    Allowed the add method to handle an unkown number of argumments/numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -29,4 +29,9 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+    @Test
+    public void testUnknownNumbers(){
+    	assertEquals(28, Calculator.add("1,2,3,4,5,6,7"));
+    }
+
 }
