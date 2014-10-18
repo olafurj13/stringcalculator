cc8dded3615c4d3cf87c269f353bf1435d046f6c
Ólafur Helgi Jónsson <olafur9@gmail.com>
2014-10-18 00:45:04 +0000

    Allowed the add method to handle a different delimiter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,4 +1,6 @@
 package is.ru.stringcalculator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
@@ -7,6 +9,9 @@ public class Calculator {
 			return 0;
 		}
 		else if(text.contains(",")){
+			return sum(splitNumbers(text));
+		}
+		else if(text.contains("/")){
 			return sum(splitNumbers(text));
 		}
 		else
@@ -18,9 +23,21 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",|\n");
+	    if(numbers.startsWith("//")){
+	    	Matcher m = Pattern.compile("//(.)\n(.*)").matcher(numbers);
+	    	m.matches();
+	    	String delimiter = m.group(1);
+	    	String num = m.group(2);
+	    	return num.split(delimiter);
+	    } else {
+	    	return numbers.split(",|\n");
+		}
 	}
-      
+
+	private static String[] splitByDelimiter(String numbers){
+
+	}
+    
     private static int sum(String[] numbers){
  	    int total = 0;
         for(String number : numbers){
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -39,4 +39,9 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1\n2,3"));
     }
 
+    @Test
+    public void testDifferentDelimiter(){
+    	assertEquals(3, Calculator.add("//;\n1;2"));
+    }
+
 }
